03a593d14e3c6040df340f292a1369772cecbbb2
Marcelo Franco <marcelodf12@gmail.com>
2020-08-15 20:46:42 -0400

    CORS configuration

--- src/main/java/py/com/buybox/trackingSystem/TrackingSystemApplication.java
+++ src/main/java/py/com/buybox/trackingSystem/TrackingSystemApplication.java
@@ -2,12 +2,29 @@ package py.com.buybox.trackingSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TrackingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrackingSystemApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.maxAge(3600);
+			}
+
+		};
 	}
 
 }
