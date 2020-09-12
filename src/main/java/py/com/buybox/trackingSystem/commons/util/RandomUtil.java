5946dc8998f57c27df76f523ca5862209dde82e1
Marcelo Franco <marcelodf12@gmail.com>
2020-09-12 17:15:18 -0400

    generar random sin simbolos

--- src/main/java/py/com/buybox/trackingSystem/commons/util/RandomUtil.java
+++ src/main/java/py/com/buybox/trackingSystem/commons/util/RandomUtil.java
@@ -4,10 +4,20 @@ import java.util.Random;
 
 public class RandomUtil {
 
+    public static Random random = new Random();
+
     public static String stringRandom(int length){
         char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_*-".toCharArray();
+        return randStr(chars, length);
+    }
+
+    public static String stringRandomWithoutSymbols(int length){
+        char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
+        return randStr(chars, length);
+    }
+
+    private static String randStr(char[] chars, int length){
         StringBuilder sb = new StringBuilder(length);
-        Random random = new Random();
         for (int i = 0; i < length; i++) {
             char c = chars[random.nextInt(chars.length)];
             sb.append(c);
